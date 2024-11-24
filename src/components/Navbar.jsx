730ff6b0d4a95c06a4b3a73bdc5c4567157237bf
thuythanh04 <thanhthuythuyp@gmail.com>
2024-11-24 14:11:45 +0700

    adjust navbar + add logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,65 @@
+import logo from '../assets/logo.png';
+
 const Navbar = () => {
   return (
-    <nav className="navbar bg-body-tertiary">
+    // <nav className="navbar bg-body-tertiary">
+    <nav
+      className="navbar bg-body-tertiary"
+      style={{
+        position: 'fixed',
+        zIndex: 1050,
+        top: 0,
+        left: 0,
+        width: '100%',
+      }}
+    >
       <div className="container px-5">
         <a className="navbar-brand" href="#">
           <img
-            src="https://tokyodelihome.com.vn/Data/Sites/1/skins/default/img//logo.png" alt="Logo"
-            width="126" height="78" className="d-inline-block align-text-top"
+            // src="https://tokyodelihome.com.vn/Data/Sites/1/skins/default/img//logo.png" alt="Logo"
+            src={logo} alt="Logo"
+            width="200" height="95" className="d-inline-block align-text-top"
+            style={{
+              marginTop: '-15px',
+            }}
           />
         </a>
 
         <form className="d-flex w-50" role="search">
           <input
-            className="form-control-md me-2 w-75" type="search" placeholder="Bạn cần tìm sản phẩm gì?"
-            aria-label="Search" />
-          <button className="btn btn" type="submit">
+            className="form-control-md me-1.5 w-75" type="search" placeholder=" Bạn cần tìm sản phẩm gì?"
+            aria-label="Search" 
+            style={{
+              border: '2px solid red',
+              outline: 'none',
+              marginLeft: '107px',
+            }}
+            />
+          <button 
+            className="btn btn" 
+            type="submit" 
+            style={{
+              border: 'none'
+            }}
+          >
             <svg
-              xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-search"
+              xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="red" className="bi bi-search"
               viewBox="0 0 16 16">
               <path
                 d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
             </svg>
           </button>
         </form>
-
-        <button type="button" className="btn position-relative">
+        
+        <div className="d-flex justify-content-end align-items-center"></div>
+        <button type="button" className="btn position-relative ms-auto">
           <svg
             xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-cart3"
             viewBox="0 0 16 16">
             <path
               d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l.84 4.479 9.144-.459L13.89 4zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
           </svg>
-          <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+          <span className="position-absolute top-0 start-95 translate-middle badge rounded-pill bg-danger">
           12
           <span className="visually-hidden">unread messages</span>
         </span>
@@ -38,7 +67,7 @@ const Navbar = () => {
 
         <div className="btn-group">
           <button
-            type="button" className="btn dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+            type="button" className="btn dropdown-toggle ms-3" data-bs-toggle="dropdown" aria-expanded="false">
             <svg
               xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor"
               className="bi bi-person-square" viewBox="0 0 16 16">
--- src/components/NavbarDown.jsx
+++ src/components/NavbarDown.jsx
@@ -8,7 +8,18 @@ const NavbarDown = () => {
   };
 
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    // <nav className="navbar navbar-expand-lg bg-body-tertiary">
+     <nav
+      className="navbar navbar-expand-lg bg-body-tertiary"
+      style={{
+        position: 'fixed',
+        zIndex: 1049,
+        top: '105px', 
+        left: 0,
+        right: 0,
+        boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)', 
+      }}
+    >
       <div className="container px-5">
         <select className="form-select w-25" aria-label="Select Branch">
           <option selected>Chọn Chi Nhánh</option>
@@ -16,7 +27,7 @@ const NavbarDown = () => {
           <option value="2">242 - 244 Phan Xích Long, Q. Phú Nhuận</option>
           <option value="3">31 - 33 Nguyễn Thị Thập, Him Lam, Q.7</option>
         </select>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="collapse navbar-collapse ms-4" id="navbarNavAltMarkup">
           <div className="navbar-nav">
             <a
               className={`nav-link ${activeNav === 0 ? 'text-danger' : ''}`}
@@ -55,6 +66,18 @@ const NavbarDown = () => {
               <b>LIÊN HỆ</b>
             </a>
           </div>
+          <button className="btn btn-outline-danger ms-auto d-flex align-items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-file-earmark-text me-2"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="M9 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V5l-3-3zM4 1h5v4h4v10H4V1z" />
+            <path
+              d="M5 7h4v1H5zM5 9h4v1H5zM5 11h4v1H5z" />
+          </svg>
+          Kiểm tra đơn hàng
+        </button>
         </div>
       </div>
     </nav>
